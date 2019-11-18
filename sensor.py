bd0b6ee143eebf7789c5e50d7d97b72ed0cd4e04
Wendy Z <54825608+OCoderO@users.noreply.github.com>
2019-11-18 12:43:44 -0600

    Nov/18/2019
    Writing to a CSV file for an excel sheet with T, Hum, and time on it.

--- sensor.py
+++ sensor.py
@@ -2,17 +2,24 @@ import time
 import board
 import busio
 import adafruit_sht31d
+import csv
+from datetime import datetime  #storeage for time 
+ 
  
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
  
 loopcount = 0
-while True:
-    print("\nTemperature: %0.1f C" % sensor.temperature)
-    print("Humidity: %0.1f %%" % sensor.relative_humidity)
-    loopcount += 1
-    time.sleep(2)
+
+with open('Sensor_data.csv', mode='w') as J:
+ data_writer.writerow(['Time','Temperature', 'Relative Humidity'])
+
+ while True:
+     print("\nTemperature: %0.1f C" % sensor.temperature)
+     print("Humidity: %0.1f %%" % sensor.relative_humidity)
+     loopcount += 1
+     time.sleep(2)
     # every 10 passes turn on the heater for 1 second
     #if loopcount == 10:
         #loopcount = 0
@@ -21,3 +28,8 @@ while True:
         #time.sleep(1)
         #sensor.heater = False
          #print("Sensor Heater status =", sensor.heater)
+
+
+    data_writer = csv.writer(J, delimiter=',')
+    data_writer.writerow([datetime.now(), sensor.temperature,sensor.relative_humidity]）
+
